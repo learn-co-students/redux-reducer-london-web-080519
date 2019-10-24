44f399a43c6b6dd343b6b5ae8dd11a98520e03e6
github username <aude53@gmail.com>
2019-10-24 13:52:43 +0100

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,13 @@
-export function manageFriends(state, action){
+export function manageFriends(state = { friends: [] }, action) {
+  if (action.type === "ADD_FRIEND") {
+    let newState = { ...state, friends: state.friends.push(action.friend) }
+  }
+  if (action.type === "REMOVE_FRIEND") {
+    let newState = {
+      ...state,
+      friends: state.friends.filter(friend => friend.id !== action.id)
+    }
+    return newState
+  }
+  return state
 }
