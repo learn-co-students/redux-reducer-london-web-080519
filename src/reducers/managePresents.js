f72aeb5aba20a842ea8530c31abe6a7d1fc2a37d
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-10-24 10:00:00 +0100

    Done.

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,19 @@
+// export function managePresents(state, action){
+//     console.log(state)
+//     switch (action.type) {
+//         case 'INCREASE':
+//             return { numberOfPresents: 1 }
+//         default: 
+//             return state;
+//     }
+// }
+
 export function managePresents(state, action){
+   // console.log(state)
+    switch (action.type) {
+        case 'INCREASE':
+            return { numberOfPresents: state.numberOfPresents + 1 }
+        default: 
+            return state;
+    }
 }
