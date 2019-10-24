6b1df7d1fff99f5e375744e5e9cf2b96288d1ee2
99chris99 <chrishodge87@gmail.com>
2019-10-24 10:13:40 +0100

    Done.

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,31 @@
 export function managePresents(state, action){
 }
+
+// state = {
+//     numberOfPresents: 0
+// }
+
+managePresents = (state, action) => {
+
+switch (action.type) {
+    case  'INCREASE':
+        return {numberOfPresents: state.numberOfPresents + 1}
+        default:
+            return {numberOfPresents: state.numberOfPresents}
+}
+
+
+}
+
+
+// function reducer(state, action){      
+//     switch (action.type) {
+//       case 'INCREASE_COUNT':
+//         return {count: state.count + 1}
+//       case 'DECREASE_COUNT':
+//         return {count: state.count - 1}
+//       default:
+//         return 
+//     }
+//   }
+  
